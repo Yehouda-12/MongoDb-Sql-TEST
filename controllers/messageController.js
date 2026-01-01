f6a4cb80581e392598e0187075586b66a21073a4
Yehouda-12 <coheny748@gmail.com>
2026-01-01 12:12:24 +0200

    bonus finish

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,3 +1,9 @@
+// RANDOM_SHUFFLE Cipher Algorithm 
+// Convert message to array of characters 
+// Repeat 5000 times: 
+// pick two different random indexes - if they are the same: redo 
+// swap their characters 
+// Join back to string 
 
 export const encryptMessage = async (req, res) => {
   try {
@@ -16,9 +22,28 @@ export const encryptMessage = async (req, res) => {
     let encryptedText = "";
     if (cipherType === "reverse") {
       encryptedText = message.split("").reverse().join("").toUpperCase();
-    } else {
-      return res.status(400).json({ message: "cipher type no good" });
+    } 
+    else if (cipherType === "ATBASH") {
+        encryptedText = atbash(message.toUpperCase());
+    } else if (cipherType === "RANDOM_SHUFFLE") {
+        let letters = message.split("");
+        for (let i = 0; i < 5000; i++) {
+            let index1 = Math.floor(Math.random() * letters.length);
+            let index2 = Math.floor(Math.random() * letters.length);
+            while (index1 === index2) {
+                index2 = Math.floor(Math.random() * letters.length);
+            }
+            let temp = letters[index1];
+            letters[index1] = letters[index2];
+            letters[index2] = temp;
+        }
+        encryptedText = letters.join("").toUpperCase();
+
+
     }
+    else {
+        return res.status(400).json({ message: "i cipher type not good" });}
+
     const [result] = await mysqlDb.query(
       `INSERT INTO messages (username,cipher_type,encrypted_text) VALUES (?,?,?)`,
       [username, cipherType, encryptedText]
@@ -36,6 +61,46 @@ export const encryptMessage = async (req, res) => {
 };
 
 
+let lookup_table = {
+  A: "Z",
+  B: "Y",
+  C: "X",
+  D: "W",
+  E: "V",
+  F: "U",
+  G: "T",
+  H: "S",
+  I: "R",
+  J: "Q",
+  K: "P",
+  L: "O",
+  M: "N",
+  N: "M",
+  O: "L",
+  P: "K",
+  Q: "J",
+  R: "I",
+  S: "H",
+  T: "G",
+  U: "F",
+  V: "E",
+  W: "D",
+  X: "C",
+  Y: "B",
+  Z: "A",
+};
+
+function atbash(message) {
+  let cipher = "";
+  for (let letter of message) {
+    if (letter !== " ") {
+      cipher += lookup_table[letter];
+    } else {
+      cipher += " ";
+    }
+  }
+  return cipher;
+}
 export const decryptMessage = async (req, res) => {
   try {
     const mysqlDb = req.mysqlConn;
@@ -63,48 +128,46 @@ export const decryptMessage = async (req, res) => {
         .split("")
         .reverse()
         .join("");
-    res.status(200).json({ id: message.id, decryptedText });
-    } else {
-        res
+      res.status(200).json({ id: message.id, decryptedText });}
+    else if (message.cipher_type === "ATBASH") {
+        decryptedText = atbash(message.encrypted_text);
+        res.status(200).json({ id: message.id, decryptedText });
+      }
+    else {
+      res
         .status(200)
         .json({ id: message.id, decryptedText: null, error: "CANNOT_DECRYPT" });
-
     }
-
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "internal server error" });
   }
 };
 
-
-export const getMessages = async (req,res)=>{
-    try {
-        const mysqlDb = req.mysqlConn;
-        const mongoDb = req.mongoDbConn;
-        const { username, password } = req.headers;
-        const usersCollection = mongoDb.collection("users");
-        const user = await usersCollection.findOne({ username, password });
-        if (!user) {
+export const getMessages = async (req, res) => {
+  try {
+    const mysqlDb = req.mysqlConn;
+    const mongoDb = req.mongoDbConn;
+    const { username, password } = req.headers;
+    const usersCollection = mongoDb.collection("users");
+    const user = await usersCollection.findOne({ username, password });
+    if (!user) {
       return res
         .status(401)
         .json({ message: "no user wiyth this name or password" });
     }
-        const [rows] = await mysqlDb.query(
-        `SELECT * FROM messages WHERE username = ?`,
-        [username]
+    const [rows] = await mysqlDb.query(
+      `SELECT * FROM messages WHERE username = ?`,
+      [username]
     );
-        const items = rows.map(row=>({
-            id: row.id,
-            cipherType: row.cipher_type,
-            encryptedText: row.encrypted_text
-        }));
-        res.status(200).json({ items });
-
-
-    } catch (err){
-        console.log(err);
-        res.status(500).json({ message: "internal server error" });
-        
-    }
-}
+    const items = rows.map((row) => ({
+      id: row.id,
+      cipherType: row.cipher_type,
+      encryptedText: row.encrypted_text,
+    }));
+    res.status(200).json({ items });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "internal server error" });
+  }
+};
